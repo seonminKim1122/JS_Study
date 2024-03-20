ac245e340244064af9cb739571ad5e3bf29bd293
김선민 <w.hand715@gmail.com>
2024-03-20 16:34:23 +0900

    IF and QuestionMark

---
+++ 02_Basic/IF_and_QuestionMark/assignment/assignment.js
@@ -0,0 +1,33 @@
+// 과제 1
+// if ("0") {
+//   alert('Hello'); // 문자열 "0" 는 treu 이므로 알림창이 뜬다
+// }
+
+// 과제 2
+// let input = prompt('자바스크립트의 공식 이름은 무엇일까요?')
+// if (input == 'ECMAScript') {
+//   alert('정답입니다!')
+// } else {
+//   alert('모르셨나요? 정답은 ECMAScript 입니다!')
+// }
+
+
+// 과제 3
+// let num = prompt('숫자를 입력해주세요')
+// if (num > 0) {
+//   alert(1);
+// } else if (num < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+
+// 과제 4
+// let result = (a + b < 4) ? '미만' : '이상';
+
+// 과제 5
+let message = (login=='직원') ? '안녕하세요.': 
+(login=='임원') ? '환영합니다.': 
+(login=='') ? '로그인이 필요합니다.': 
+'';

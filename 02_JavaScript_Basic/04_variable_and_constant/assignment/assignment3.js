d4d907062865f6ca4abc5bbdde4553edcaac4b5a
김선민 <w.hand715@gmail.com>
2024-04-15 17:14:41 +0900

    JS Study - JAVASCRIPT BASIC day 1

---
+++ 02_JavaScript_Basic/04_variable_and_constant/assignment/assignment3.js
@@ -0,0 +1,17 @@
+/*
+아래 코드를 평가해보시기 바랍니다.
+
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+
+
+상수명은 해당 상수의 값이 언제 결정되느냐에 따라 일반적인 camelCase 를 따를 것을 권장하기도,
+대문자 상수를 사용할 것을 권장하기도 합니다.
+
+하드코딩하는 경우 대문자 상수를, 런타임시 결정되는 경우 camelCase 를 권장하므로
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+
+와 같이 수정하는 것이 좋습니다.
+*/
---
+++ 02_JavaScript_Basic/05_datatype/assignment/assignment.js
@@ -0,0 +1,6 @@
+// 아래 스크립트의 결과를 예측
+let name = 'Ilya';
+
+alert(`hello ${1}`); // hello 1
+alert(`hello ${"name"}`); // hello name
+alert(`hello ${name}`); // hello Ilya
